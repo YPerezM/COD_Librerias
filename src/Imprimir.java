0504569f8fb762155ba4d1d96f7f0f7773665c81
Administrador <Administrador@LENOVO-842AF4C6>
2015-02-09 09:16:22 +0100

    COmmit inicial

---
+++ src/Imprimir.java
@@ -0,0 +1,14 @@
+
+
+/**
+ *
+ * @author Administrador
+ */
+public class Imprimir {
+    
+    public static void saludo(){
+        System.out.println("Hola desde Library");
+        
+    }
+
+}
---
+++ src/MiPaquete/Imprimir.java
@@ -0,0 +1,14 @@
+
+
+package MiPaquete;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Imprimir {
+    public static void saludo(){
+        System.out.println("Hola desde Library");
+        
+    }
+}
